8a260cae1d0684203ba3043d48327f6e5b65c291
JonathanL617 <jonathanlmx13@gmail.com>
2025-10-02 11:57:04 +0800

    create mvc

--- dashboard.php
+++ dashboard.php
@@ -2,9 +2,45 @@
 <html>
     <head>
         <!-- css -->
+        <link rel="stylesheet" href="css/dashboard.css">
     </head>
     <body>
         <!-- navigation bar -->
+        <nav class="nav-conatiner">
+            <div class="tab-container">
+                <!-- switch content of the tabs based on user role -->
+                <?php switch($userRole){
+                    case 'superadmin': ?>
+                        <button class="tab">Manage Accounts</button>
+                        <button class="tab">Requests</button>
+                    <?php break;
+
+                    case 'admin': ?>
+                        <button class="tab">Manage Users</button>
+                        <button class="tab">Regiter Patients</button>
+                        <button class="tab">Account Requests</button>
+                    <?php break;
+
+                    case 'pharmacist': ?>
+                        <button class="tab">Medicine Stock</button>
+                        <button class="tab">Precription Requests</button>
+                        <button class="tab">Precsription Queue</button>
+                        <button class="tab">Dispense History</button>
+                    <?php break;
+
+                    case 'doctor': ?>
+                        <button class="tab">Patient Records</button>
+                        <button class="tab">Prescriptions</button>
+                        <button class="tab">Resupply Requests</button>
+                        <button class="tab">History</button>
+                    <?php break;
+                } ?>
+                
+            </div>
+            <div class="profile">
+
+            </div>
+        </nav>
         <!-- four role-based view -->
         <!-- main content -->
         <!-- content loader -->
@@ -42,4 +78,5 @@
             manage resupply request for patient (approve/reject)
         -->
     </body>
+    <!-- js -->
 </html>
--- login_page.php
+++ login_page.php
@@ -7,9 +7,10 @@
 
 <html>
     <head>
-        <style>
-
-        </style>
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="css/login_page.css">
     </head>
     <body>
         <div class="layout">
@@ -35,5 +36,6 @@
 
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
